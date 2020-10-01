681385be0f588b557008979f91f88f0f74267248
Rakhi Pareek <63310519+Rakhipareek@users.noreply.github.com>
2020-10-01 13:49:08 +0700

    Update if_else.c

--- if_else.c
+++ if_else.c
@@ -3,12 +3,14 @@ int main() {
     double num;
     printf("Enter a number: ");
     scanf("%lf", &num);
-    if (num <= 0.0) {
-        if (num == 0.0)
-            printf("You entered 0.");
-        else
-            printf("You entered a negative number.");
-    } else
+    if((num < 0.0)||(num==0.0))
+    {
+            printf("You entered 0. or negative number");
+   
+    } 
+    else
+    {
         printf("You entered a positive number.");
+    }
     return 0;
 }
