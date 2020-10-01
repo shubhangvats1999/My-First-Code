0aea16192a212c5a3b24989258609c68ea98d734
Rakhi Pareek <63310519+Rakhipareek@users.noreply.github.com>
2020-10-01 14:07:57 +0700

    Update size of operator.c

--- size of operator.c
+++ size of operator.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main() {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
+    int b=10;
+    float c=10.0;
+    double d=10.15;
+    char e='A';
 
     // sizeof evaluates the size of a variable
-    printf("Size of int: %ld bytes\n", sizeof(intType));
-    printf("Size of float: %ld bytes\n", sizeof(floatType));
-    printf("Size of double: %ld bytes\n", sizeof(doubleType));
-    printf("Size of char: %ld byte\n", sizeof(charType));
+    printf("Size of int: %ld bytes\n", sizeof(b));
+    printf("Size of float: %ld bytes\n", sizeof(c));
+    printf("Size of double: %ld bytes\n", sizeof(d));
+    printf("Size of char: %ld byte\n", sizeof(e));
     
     return 0;
 }
